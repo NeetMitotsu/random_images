8d096e2d892aa56c0a9a1511fc5832531fc612dd
lxd_workPC <lxd3808@163.com>
2018-05-16 16:13:05 +0800

    根据配置文件修改参数， response返回随机图片

--- images.py
+++ images.py
@@ -1,52 +1,71 @@
 from flask import Flask
 from flask import request
-from flask import make_response
+from flask import Response
 import json
 import time
 from urllib.parse import urlparse
+from imgdata import Imgdata
 
 import os
 import re
+import configparser
+import random
 
 app = Flask(__name__)
-
-@app.route('/image')
-def image():
-	headers = request.headers
-	# 来源
-	referer = headers['referer']
-	if referer:
-		refererhost = urlparse(referer)
-		host = refererhost.netloc.lower()
-		status = checkReferer(host)
-	else:
-		pass
+# print(os.path.abspath('.'))
+config = configparser.ConfigParser()
+config.read('./resource/profile.ini')
+allowFiles = config.get('conf', 'imageAllowFiles')
+imageManagerListPath = config.get('conf', 'imageAllowFiles')
+basePath = config.get('conf', 'basePath')
+category = config.get('conf', 'category')
 
 
-def getfiles(dirpath:str, allowFiles:list, files:list = list()):
-	"""
-	获取图片文件名列表
-	"""
-	if not os.path.isdir(dirpath):
-		return None;
-	if dirpath[-1] != '\\':
-		dirpath = dirpath + '\\'
-	for filePath in os.listdir(dirpath):
-		path2 = os.path.join(dirpath, filePath)
-		if os.path.isdir(filePath):
-			getfiles(path2, allowFiles, files)
-		else:
-			# "/\.(" . $allowFiles . ")$/i"
-			if re.match(".(".join(allowFiles).join(")$/i"), filePath):
-				files.append(path2)
-	return files
-
-def checkReferer(url, domainList = ['www.yuudati.com','www.jty127.com']):
-	"""检查来源网址"""
-	status = False
-	refer = ""
-	if refer in domainList:
-		status = True
-	return status
+@app.route('/image/<string:category>', methods=['GET', 'POST'])
+def image(category):
+    # 没有相对路径， 返回空
+    if not category:
+        return Response(status=404)
+    # 获取文件列表
+    filelist = getfiles(category, allowFiles)
+    if not filelist:
+        return Response(status=404)
+    count = len(filelist)
+    # 列表下标随机数
+    rand = random.choice(range(count))
+    imgpath = filelist[rand]
+    img = Imgdata(imgpath)
+    info = img.data2img()
+    # response.headers['constent-type'] = info['content-type']
+    return Response(info['data'], mimetype=info['content-type'])
 
 
+def getfiles(category: str, allowFiles: str, files: list = list()):
+    """
+    获取图片文件名列表
+    """
+    path = os.path.abspath('.')
+    path = os.path.join(path, basePath, category)
+    if not os.path.isdir(path):
+        return None
+    for filePath in os.listdir(path):
+        path2 = os.path.join(path, filePath)
+        if os.path.isdir(filePath):
+            getfiles(path2, allowFiles, files)
+        else:
+            if re.match('.*(' + allowFiles + ')$', filePath, re.S):
+                files.append(path2)
+    return files
+
+
+def checkReferer(url, domainList=['www.yuudati.com', 'www.jty127.com']):
+    """检查来源网址"""
+    status = False
+    refer = ""
+    if refer in domainList:
+        status = True
+    return status
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', debug=True)
--- imgdata.py
+++ imgdata.py
@@ -1,63 +1,65 @@
 import magic
 import os
 
+
 class Imgdata(object):
-	__slots__ = ('__imgsrc', '__imgdata', '__imgform')
-	"""docstring for Imgdata"""
-	def __init__(self, imgsrc):
-		super(Imgdata, self).__init__()
-		self.__imgsrc = imgsrc
-		filesize = os.path.getsize(self.__imgsrc)
-		with open(self.__imgsrc, 'rb') as rb:
-			#保存字节数组数据
-			self.__imgdata = rb.read(filesize)
-		# self.__imgdata = imgdata
-		#初始化数据，保存imgform（mime信息即content-type）
-		mime = magic.Magic(mime=True)
-		self.__imgform = mime.from_file(imgsrc)
+    """docstring for Imgdata"""
 
-	# def img2data():
-	# 	# self._imgform(self.__imgsrc)
-	# 	filesize = os.path.getsize(self.__imgsrc)
-	# 	with open(self.__imgsrc, 'rb') as rb:
-	# 		self.__imgdata = rb.read(filesize)
+    __slots__ = ('__imgsrc', '__imgdata', '__imgform')
 
-	def data2img(self):
-		"""
-		返回文件content-type与字节数组data 字典
+    def __init__(self, imgsrc):
+        super(Imgdata, self).__init__()
+        self.__imgsrc = imgsrc
+        filesize = os.path.getsize(self.__imgsrc)
+        with open(self.__imgsrc, 'rb') as rb:
+            # 保存字节数组数据
+            self.__imgdata = rb.read(filesize)
+        # self.__imgdata = imgdata
+        # 初始化数据，保存imgform（mime信息即content-type）
+        mime = magic.Magic(mime=True)
+        self.__imgform = mime.from_file(imgsrc)
 
-		:return dict
-		"""
-		info = {};
-		info['content-type'] = self.__imgform
-		info['data'] = self.__imgdata
-		return info
-		
+    # def img2data():
+    # 	# self._imgform(self.__imgsrc)
+    # 	filesize = os.path.getsize(self.__imgsrc)
+    # 	with open(self.__imgsrc, 'rb') as rb:
+    # 		self.__imgdata = rb.read(filesize)
 
-	# def _imgform():
-	# 	mime = magic.Magic(mime=True)
-	# 	self.__imgform = mime.from_file(self.__imgsrc)
+    def data2img(self):
+        """
+        返回文件content-type与字节数组data 字典
 
-	@property
-	def imgsrc(self):
-		return self.__imgsrc
+        :return: keys: content-type, data
+        """
+        info = {};
+        info['content-type'] = self.__imgform
+        info['data'] = self.__imgdata
+        return info
 
-	@imgsrc.setter
-	def imgsrc(self, value):
-		self.__imgsrc = value
+    # def _imgform():
+    # 	mime = magic.Magic(mime=True)
+    # 	self.__imgform = mime.from_file(self.__imgsrc)
 
-	@property
-	def imgdata(self):
-		return self.__imgdata
+    @property
+    def imgsrc(self):
+        return self.__imgsrc
 
-	@imgdata.setter
-	def imgdata(self, value):
-		self.__imgdata = value
+    @imgsrc.setter
+    def imgsrc(self, value):
+        self.__imgsrc = value
 
-	@property
-	def imgform(self):
-		return self.__imgform
+    @property
+    def imgdata(self):
+        return self.__imgdata
 
-	@imgform.setter
-	def imgform(self, value):
-		self.__imgform = value
+    @imgdata.setter
+    def imgdata(self, value):
+        self.__imgdata = value
+
+    @property
+    def imgform(self):
+        return self.__imgform
+
+    @imgform.setter
+    def imgform(self, value):
+        self.__imgform = value
